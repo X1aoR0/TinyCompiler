61269cb6eb543c88791c66364c37411802e478b0
X1aoR0 <13652223526@qq.com>
2020-02-08 21:44:22 +0800

    添加语法分析器

---
+++ scanner.py
@@ -0,0 +1,109 @@
+import string
+
+TOKEN_TYPE = ['PLUS','MINUS','TIMES','DIV','LT','EQ','LPARAM','RPARAM','SEMI',
+              "ID",'NUM',
+              'IF','THEN','ELSE','END','REPEAT','UNTIL','READ','WRITE',
+              'ENDFILE','ERROR']
+
+
+WHILE_SPACE = [" ","\t"]
+DIGIT = string.digits
+SPECIAL_SYMBOLS = {'+':'PLUS','-':'MINUS','*':"TIMES",'/':'DIV','<':'LT','=':'EQ','(':'LPARAM',')':'RPARAM',';':'SEMI'}
+LETTER = string.ascii_letters
+REVERSED_WORDS = {'if':'IF','then':'THEN','else':'ELSE','end':'END','repeat':'REPEAT',
+                  'until':'UNTIL','read':'READ','write':'WRITE'}
+
+srcfile = open("sample.tny","r")
+srclines = srcfile.readlines()
+# print(srclines)
+srcfile.close()
+
+cur_line = 0
+
+state = 'START'
+token_type = ""
+token_value = ""
+for line in srclines:
+    cur_index = -1
+    cur_line += 1
+    print(str(cur_line)+":",end="")
+    print(line)
+    while True:
+        if state == 'START':
+            if line[cur_index+1] == '\n':
+                break
+            if line[cur_index+1] == '{':
+                cur_index += 1
+                state = 'IN_COMMENT'
+            elif line[cur_index+1] in WHILE_SPACE:
+                cur_index +=1
+            elif line[cur_index+1] in DIGIT:
+                cur_index +=1
+                state = 'IN_NUM'
+                token_value += line[cur_index]
+            elif line[cur_index+1] == ':':
+                cur_index +=1
+                state = 'IN_ASSIGN'
+                token_value += line[cur_index]
+            elif line[cur_index+1] in SPECIAL_SYMBOLS.keys():
+                cur_index +=1
+                token_type = SPECIAL_SYMBOLS[line[cur_index]]
+                state = 'DONE'
+                token_value += line[cur_index]
+            elif line[cur_index+1] in LETTER:
+                cur_index +=1
+                token_type = 'ID'
+                state = 'IN_ID'
+                token_value += line[cur_index]
+        elif state == 'IN_COMMENT':
+            if line[cur_index+1] == '}':
+                cur_index += 1
+                state = 'START'
+            elif line[cur_index+1] == '\n':
+                break
+            else:
+                cur_index += 1
+
+        elif state == 'IN_NUM':
+            if line[cur_index+1] in DIGIT:
+                cur_index += 1
+                state = 'IN_NUM'
+                token_value += line[cur_index]
+            elif line[cur_index+1] not in DIGIT:
+                state = 'DONE'
+
+        elif state == 'IN_ID':
+            if line[cur_index+1] in LETTER:
+                cur_index +=1
+                state = 'IN_ID'
+                token_value += line[cur_index]
+            else:
+                if token_value in REVERSED_WORDS.keys():
+                    token_type = REVERSED_WORDS[token_value]
+                state = 'DONE'
+        elif state == 'IN_ASSIGN':
+            if line[cur_index+1] =='=':
+                cur_index +=1
+                token_value += line[cur_index]
+                state = 'DONE'
+            else:
+                state = 'ERROR'
+        if state == 'DONE':
+            print(str(cur_line)+":",end="")
+            print(token_type + ':' + token_value)
+            token_type = ''
+            token_value = ''
+            state = 'START'
+            if line[cur_index+1] == '\n':
+                break
+
+        if state == 'ERROR':
+            print("ERROR:"+ str(cur_line)+','+str(cur_index))
+            break
+
+# #处理最后一个token
+# if state != 'START' and state != 'ERROR':
+#     print(str(cur_line) + ":", end="")
+#     print(token_type + ':' + token_value)
+#     token_type = ''
+#     token_value = ''
